ac4628f52efb3a6cb97f51f75837fafd28ed3a5d
daynaclowe <daynalowe@Dayna-Lowes-MacBook.local>
2014-10-21 14:04:11 -0400

    It's my party and I'll cry if I want to.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Herro #{name}."
+
+puts "Oh! I heard it's your birthday #{name}, how old are you today?"
+age = gets.chomp
+puts "Well #{name}, #{age} is a ripe old age. Happy birthday, Baby."
